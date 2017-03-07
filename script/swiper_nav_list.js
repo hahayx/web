dc553239dc5da91ffa0fa054b39d9ec243fc8d9a
liguanjian@rd.com <liguanjian@rd.com>
2017-03-07 21:44:45 +0800

    滑动功能修改bug

---
+++ script/swiper_nav_list.js
@@ -0,0 +1,11 @@
+/**
+ * Created by liguanjian on 2017-3-7.
+ */
+var swiper = new Swiper('.swiper-container', {
+    slidesPerView: 'auto',
+    centeredSlides: false,
+    paginationClickable: false,
+    spaceBetween: 0,
+    // slidesOffsetAfter : 46,
+    grabCursor: false
+});
